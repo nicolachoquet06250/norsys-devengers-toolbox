e8ce20cb118878af189d5606aa68777f7791a417
Nicolas Choquet <nchoquet@norsys.fr>
2023-06-01 14:36:35 +0200

    Debug

--- gui/helpers.go
+++ gui/helpers.go
@@ -7,7 +7,6 @@ import (
 	"norsys-devengers-toolbox/environement"
 	"os"
 	"strconv"
-	"strings"
 
 	"github.com/asticode/go-astilectron"
 )
@@ -19,8 +18,8 @@ func UrlBase() string {
 }
 
 func IsDevEnv() bool {
-	return os.Getenv("OPEN_DEVTOOLS") == "1" &&
-		strings.Contains(os.Args[0], environement.Slash()+"b001"+environement.Slash()+"exe"+environement.Slash())
+	return os.Getenv("OPEN_DEVTOOLS") == "1" /*&&
+	strings.Contains(os.Args[0], environement.Slash()+"b001"+environement.Slash()+"exe"+environement.Slash())*/
 }
 
 func OpenDevTools(w *astilectron.Window, l *log.Logger) {
